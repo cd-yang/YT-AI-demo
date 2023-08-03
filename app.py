91730d3119cd342e6fad124e56d46bd7484d77a0
CD Yang <cdyang1991@gmail.com>
2023-08-03 21:35:28 +0800

    init commit by lilongyang

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+import json
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", title='Home')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # 检查是否有文件被上传
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    # 如果用户没有选择文件，浏览器也会提交一个空的部分，所以需要检查文件是否存在名字
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    # 读取文件内容
+    content = file.read().decode('utf-8')
+
+    content = content.replace("\n", "")
+    content = content.replace("\r", "")
+    content = content.replace("-", "")
+    content = content.replace("=", "")
+    import gpt
+    print("="*10+"正在询问gpt"+"="*10)
+    json_data = gpt.GetJson(content)
+    print("="*10+"询问完毕"+"="*10)
+    print(json_data)
+    return json_data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ gpt.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import json
+import openai
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+openai.api_base = os.environ['API_BASE']
+openai.api_type = os.environ['API_TYPE']
+openai.api_version = os.environ['API_VERSION']
+deployment_name = os.environ['DEPLOYMENT_NAME']
+
+
+def get_completion(prompt, temperature=0.9, model="gpt-35"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        engine=deployment_name
+    )
+    return response.choices[0].message["content"]
+
+
+def GetJson(description):
+    prompt = f'''
+    Your task is to help a system engineer create a 
+    system model based 
+    on a technical fact sheet.
+
+    Write all of the entities needed in the system model based on the information 
+    provided in the technical specifications delimited by 
+    triple backticks.
+
+    Use the following format:
+    ENTITIES: <text to summarize>
+    ACTIONS: <summary>
+    Output JSON: <json with summary and num_names>
+    
+    Technical specifications:  ```{description}```
+    只需要输出Output JSON 的内容，别的都不用输出，并且要用原文不能进行翻译和修改
+    '''
+    # print(prompt)
+    return get_completion(prompt)
